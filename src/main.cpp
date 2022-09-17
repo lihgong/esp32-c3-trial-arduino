fe0e7d8d7b4978c4f755bd440a6c16c469dbffc6
Lihgong Wu <lihgong@gmail.com>
2022-09-17 13:52:58 +0800

    Arduino engineering template

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,21 @@
 #include <Arduino.h>
 
+#define LED_BUILTIN1 (12)
+#define LED_BUILTIN2 (13)
+
+// the setup function runs once when you press reset or power the board
 void setup() {
-  // put your setup code here, to run once:
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN1, OUTPUT);
+  pinMode(LED_BUILTIN2, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(LED_BUILTIN1, LOW);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN2, HIGH);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN2, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
