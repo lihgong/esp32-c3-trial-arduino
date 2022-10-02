0025ccfd481653642cb86d5416c46581367c1184
Lihgong Wu <lihgong@gmail.com>
2022-10-02 19:47:17 +0800

    Add serial port supporting in the framework

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN1, OUTPUT);
   pinMode(LED_BUILTIN2, OUTPUT);
+  Serial.begin(); // open the serial port at 9600 bps:
 }
 
 // the loop function runs over and over again forever
@@ -18,4 +19,5 @@ void loop() {
   digitalWrite(LED_BUILTIN1, HIGH);   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN2, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
+  Serial.printf("QQM\n"); // print something
 }
